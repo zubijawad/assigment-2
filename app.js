a8b8912f1d511326b489ba64a9031c1ea10ec902
zubijawad <zubijawad495@gmail.com>
2025-06-21 10:19:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+// chapter 3
+// answer 1
+var age = 15;
+alert(" i am "+ age +"Years old");
+// answer 2
+var visitcount = 14;
+doucument.write("you have visited this site" + visitcount + " times");
+// answer 3
+var birthyear =1995;
+document.write("My birth yera is "+ 1995 + "<br>");
+document.write("Data type of my ddeclear Number" + typeof 1995);
+// answer 4
+// a . visitors name
+var visitorName = "john Doe";
+// b . product title
+var oroductTitle = "T-shirt";
+// c . Quantity
+var quantity = 5;
+document.write(visitorName + " ordered" + quantity + "" +productTitle + "(s) on XYZ Clothing store.");
+// chapter 4
+// answer 1
+var a, b, c;
+// answer 2
+// LegalVariable Names;
+ myVariable
+ data-2025
+ userName
+ totalAmount
+//  lllegal Variable Names;
+myVariable
+my-Variable
+class
+user 
+// answer 3
+<h1>Rules for naming JS variables</h1>
+ var Smy_1stVariable
+$
+case-sensitive
+JS keywords
+// chapter 5
+// answer 1
+var num1 = 3;
+var num2 = 5;
+var sum = num1 + num2;
+var resuitparagraph = document.write("resuit");
+resultparagraph.textcontent = "sum of" + num1 + "and" + "is" + sum;
+// answer 2
+-
+*
+/
+%
+2-5
+5*5
+3/6
+5%7
+// answer number3
+var myVariable;
+document.write("value after variable declaration is:" + myVariable + "<br>");
+myVariable = 5;
+myVariable++;
+this will change myvariable from 5to 6
+myVariable += 7;
+if myVariable was 6after incrementing, it will become 13 (6 + 7)
+document.write("value after adding 7 is: " + myVariable + "<br>");
+output: value after adding 7 is: 13
+13-1 12
+"value after decrement is:12"
+divide the decremented value by 3:
+12 divide  3 =4 with a remainder of o
+"The remainder is: o"
+// answer 4
+var ticket_price = 600(PKR)
+total_cost = ticket_price * 5
+total_cost = 600 * 5 = 3000
+The total cost ofbuying 5 movie tickets is 3000 PKR
+// answer 6
+celsius_temp = 25
+use the formila = (c*9/5)+32
+valculate it_temp = (celsius_temp *9/5) + 32
+fahrenheit_temp_input + 70
+use the formula:c =(f-32)*5/9
+caculate celsius_temp_output = (fahrenheit_temp_input - 32)*5/9
+output the result in yhe specified format:
+"70 F is 21.1111 c"
+// answer7
+var priceItem1 = 10.00;
+var priceItem2 = 25.50;
+var quantityItem1 = 2;
+var quantityItem2 = 1;
+var shippingCharges = 5.00;
+let totalcost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) +shippingCharges;
+price of item 1 * quantity of item 1 =
+650*3=1950
+price of item 2 * quantity of item 2 =
+100*7=700
+cost of item 1 + cost of item 2=
+1950+700=2650
+// answer8
+subtotal+shipping charges=
+2650+100=2750
+total:2750
+the percentage is calculated by dividing the marks obtained by the total marks and multiply by 100:
+(804/980)*100 =82.0408163265306%
+
+
+
+
+
+
